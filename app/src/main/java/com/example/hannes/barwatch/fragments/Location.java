5eb890f9777ad11b6e6dbfc83f01850c4dc4c88d
ChrisVorpsn89 <CVorpahl@gmx.de>
2016-09-15 12:02:39 +0200

    Google Maps fixed

--- app/src/main/java/com/example/hannes/barwatch/fragments/Location.java
+++ app/src/main/java/com/example/hannes/barwatch/fragments/Location.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import com.example.hannes.barwatch.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,19 +23,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
  */
 public class Location extends Fragment implements OnMapReadyCallback {
 
-    /**
-     * String [] SPINNERLIST = {"Mood","KA5PER","Hemingway's","U-Bar","Pony","Jag deine Eltern nicht vom Hof","Alte Filmbühne",
-     * "Banane", "Heimat", "Barock Bar", "Franky's Sportsbar","Sax", "Piratenhöhle" , "Orange Club" , "Da Silva", "Rauschgold",
-     * "Ernstl","Suite 15","Beats", "Gatsby","Jalapenos","No. 7","Picasso","Falnnigans", "Bar 13","Upper Club 21", "Escobar",
-     * "Zarap Zap Zap", "Magaritas","Max Bar", "Café Felix","Hexenhäuschen", "Skala"};
-     **/
-
     SupportMapFragment sMapFragment;
     private GoogleMap mMap;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View v = inflater.inflate(R.layout.fragment_location, container, false);
         setUpMap();
         return v;
@@ -55,7 +48,7 @@ public class Location extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        // Move camera to Regensburg City
+        // Move camera to Regensburg Neufahrplatz
         LatLng regensburg = new LatLng(49.01849959358728, 12.0958256717131);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(regensburg));
         mMap.moveCamera(CameraUpdateFactory.zoomTo(16));
@@ -76,39 +69,39 @@ public class Location extends Fragment implements OnMapReadyCallback {
 
     private void setUpBar() {
 
-        LatLng mood = new LatLng(49.021140908053184, 12.096453308950004);
-        LatLng kasper = new LatLng(49.019007, 12.094186);
-        LatLng hemingways = new LatLng(49.018027, 12.094937);
-        LatLng ubar = new LatLng(49.017817, 12.094384);
-        LatLng pony = new LatLng(49.017901, 12.094763);
-        LatLng jagdhof = new LatLng(49.020907, 12.096582);
         LatLng alteFilm = new LatLng(49.020863, 12.097123);
         LatLng banane = new LatLng(49.020557, 12.091984);
-        LatLng heimat = new LatLng(49.020012, 12.091907);
-        LatLng barock = new LatLng(49.019971, 12.096997);
-        LatLng francky = new LatLng(49.020228, 12.098124);
-        LatLng sax = new LatLng(49.017869, 12.093132);
-        LatLng pirat = new LatLng(49.020701, 12.095359);
-        LatLng orange = new LatLng(49.021158, 12.093591);
-        LatLng daSilva = new LatLng(49.016645, 12.096161);
-        LatLng rausch = new LatLng(49.016291, 12.096930);
-        LatLng ernstl = new LatLng(49.014163, 12.101131);
-        LatLng suite = new LatLng(49.015443, 12.096517);
-        LatLng beats = new LatLng(49.015865, 12.096831);
-        LatLng gatsby = new LatLng(49.015906, 12.096622);
-        LatLng jala = new LatLng(49.017417, 12.089055);
-        LatLng noSeven = new LatLng(49.019260, 12.091813);
-        LatLng picasso = new LatLng(49.020377, 12.097649);
-        LatLng flan = new LatLng(49.021157, 12.093784);
         LatLng bar = new LatLng(49.021306, 12.092682);
-        LatLng upper = new LatLng(49.018861, 12.098035);
+        LatLng barock = new LatLng(49.019971, 12.096997);
+        LatLng beats = new LatLng(49.015865, 12.096831);
+        LatLng daSilva = new LatLng(49.016645, 12.096161);
+        LatLng ernstl = new LatLng(49.014163, 12.101131);
         LatLng esco = new LatLng(49.017396, 12.093824);
-        LatLng zap = new LatLng(49.017524, 12.095219);
+        LatLng felix = new LatLng(49.016267, 12.098014);
+        LatLng flan = new LatLng(49.021157, 12.093784);
+        LatLng francky = new LatLng(49.020228, 12.098124);
+        LatLng gatsby = new LatLng(49.015906, 12.096622);
+        LatLng heimat = new LatLng(49.020012, 12.091907);
+        LatLng hemingways = new LatLng(49.018027, 12.094937);
+        LatLng hexen = new LatLng(49.009761, 12.102276);
+        LatLng jagdhof = new LatLng(49.020907, 12.096582);
+        LatLng jala = new LatLng(49.017417, 12.089055);
+        LatLng kasper = new LatLng(49.019007, 12.094186);
         LatLng magaritas = new LatLng(49.010130, 12.102697);
         LatLng max = new LatLng(49.015070, 12.100007);
-        LatLng felix = new LatLng(49.016267, 12.098014);
-        LatLng hexen = new LatLng(49.009761, 12.102276);
+        LatLng mood = new LatLng(49.021140908053184, 12.096453308950004);
+        LatLng noSeven = new LatLng(49.019260, 12.091813);
+        LatLng orange = new LatLng(49.021158, 12.093591);
+        LatLng picasso = new LatLng(49.020377, 12.097649);
+        LatLng pirat = new LatLng(49.020701, 12.095359);
+        LatLng pony = new LatLng(49.017901, 12.094763);
+        LatLng rausch = new LatLng(49.016291, 12.096930);
+        LatLng sax = new LatLng(49.017869, 12.093132);
         LatLng skala = new LatLng(49.018895, 12.092689);
+        LatLng suite = new LatLng(49.015443, 12.096517);
+        LatLng ubar = new LatLng(49.017817, 12.094384);
+        LatLng upper = new LatLng(49.018861, 12.098035);
+        LatLng zap = new LatLng(49.017524, 12.095219);
 
         mMap.addMarker(new MarkerOptions().position(mood).title("Mood"));
         mMap.addMarker(new MarkerOptions().position(kasper).title("KA5PER Cocktailbar"));
@@ -143,6 +136,5 @@ public class Location extends Fragment implements OnMapReadyCallback {
         mMap.addMarker(new MarkerOptions().position(felix).title("Café Felix"));
         mMap.addMarker(new MarkerOptions().position(hexen).title("Hexenhäusel"));
         mMap.addMarker(new MarkerOptions().position(skala).title("Skala"));
-
     }
 }
